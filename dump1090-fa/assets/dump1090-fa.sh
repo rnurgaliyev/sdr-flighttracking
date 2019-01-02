ea66bb0bfbe7388835bb11008f3bd264bfbead82
Renat Nurgaliyev <r.nurgaliyev@syseleven.de>
2019-01-02 14:01:41 +0100

    Fix JSON output

--- dump1090-fa/assets/dump1090-fa.sh
+++ dump1090-fa/assets/dump1090-fa.sh
@@ -7,7 +7,8 @@
 RECEIVER_OPTIONS="--device-index 0 --gain -10 --ppm 0"
 DECODER_OPTIONS="--max-range 360"
 NET_OPTIONS="--net --net-ro-size 512 --net-ro-interval 1"
+JSON_OPTIONS="--write-json /run/dump1090-fa --json-location-accuracy 1"
 MISC_OPTIONS="--metric --quiet"
 
 # Run dump1090-fa
-/usr/bin/dump1090-fa $RECEIVER_OPTIONS $DECODER_OPTIONS $NET_OPTIONS $MISC_OPTIONS
+/usr/bin/dump1090-fa $RECEIVER_OPTIONS $DECODER_OPTIONS $NET_OPTIONS $JSON_OPTIONS $MISC_OPTIONS
