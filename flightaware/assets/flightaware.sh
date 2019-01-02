756b519f1c2fce11590bbcc2f2a1bca250f62cff
Renat Nurgaliyev <r.nurgaliyev@syseleven.de>
2019-01-02 15:00:16 +0100

    Fix feeder id

--- flightaware/assets/flightaware.sh
+++ flightaware/assets/flightaware.sh
@@ -3,7 +3,7 @@
 piaware-config flightaware-user $FLIGHTAWARE_USER
 piaware-config flightaware-password $FLIGHTAWARE_PASSWORD
 
-if [ -z "$FLIGHTAWARE_FEEDER_ID" ]; then
+if [ ! -z "$FLIGHTAWARE_FEEDER_ID" ]; then
   piaware-config feeder-id $FLIGHTAWARE_FEEDER_ID
 fi
 
