4a83a922ffb5c8769a530173c4231731de6a7bd3
Renat Nurgaliyev <r.nurgaliyev@syseleven.de>
2019-01-29 23:11:57 +0100

    Move json flight stats to ram drive

--- dump1090-fa/assets/dump1090-fa.sh
+++ dump1090-fa/assets/dump1090-fa.sh
@@ -10,5 +10,8 @@ NET_OPTIONS="--net --net-ro-size 512 --net-ro-interval 1"
 JSON_OPTIONS="--write-json /run/dump1090-fa --json-location-accuracy 1"
 MISC_OPTIONS="--metric --quiet"
 
+# Make sure directory for json exists
+mkdir -p /run/dump1090-fa
+
 # Run dump1090-fa
 /usr/bin/dump1090-fa $RECEIVER_OPTIONS $DECODER_OPTIONS $NET_OPTIONS $JSON_OPTIONS $MISC_OPTIONS
