c6db82e3116eb5c7a412795999e384fc1bf5bc0e
Renat Nurgaliyev <renat@rnu.one>
2025-01-12 16:06:27 +0100

    Update Dockerfiles, add support for rtl-sdr blog v4

--- dump1090-fa/assets/dump1090-fa.sh
+++ dump1090-fa/assets/dump1090-fa.sh
@@ -4,8 +4,9 @@
 /usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf
 
 # Configure options for dump1090-fa
-RECEIVER_OPTIONS="--device-index 0 --gain -10 --ppm 0"
+RECEIVER_OPTIONS="--device-index 0 --device-type rtlsdr --enable-agc --ppm 0"
 DECODER_OPTIONS="--max-range 360"
+ADAPTIVE_GAIN_OPTIONS="--adaptive-range --adaptive-burst"
 NET_OPTIONS="--net --net-ro-size 512 --net-ro-interval 1"
 JSON_OPTIONS="--write-json /run/dump1090-fa --json-location-accuracy 1"
 MISC_OPTIONS="--metric --quiet"
@@ -14,4 +15,4 @@ MISC_OPTIONS="--metric --quiet"
 mkdir -p /run/dump1090-fa
 
 # Run dump1090-fa
-/usr/bin/dump1090-fa $RECEIVER_OPTIONS $DECODER_OPTIONS $NET_OPTIONS $JSON_OPTIONS $MISC_OPTIONS
+/usr/bin/dump1090-fa $RECEIVER_OPTIONS $DECODER_OPTIONS $ADAPTIVE_GAIN_OPTIONS $NET_OPTIONS $JSON_OPTIONS $MISC_OPTIONS
